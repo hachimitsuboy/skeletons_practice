f07efb44819411a4202d931be74c93dcab45b930
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-08-05 09:48:16 +0900

    futureproviderを用いたtodoリストの変更とローカルストレージ機能の追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_practice/providers/shareProvider.dart';
 
+FutureProvider<List<String>> futureWordListProvider =
+    FutureProvider((ref) async {
+  //shared_preferencesから保存されているwordListを取り出す
+  final getList = await ref.read(shareProvider).getData();
+  ref.read(wordListProvider.notifier).state = getList;
+  return getList;
+});
+
 StateProvider<List<String>> wordListProvider = StateProvider((ref) => []);
 
 void main() {
   runApp(
-    ProviderScope(
-      child: MyApp(),
+    const ProviderScope(
+      child: const MyApp(),
     ),
   );
 }
@@ -31,14 +39,8 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<String> wordList = ref.watch(wordListProvider);
-    print("wordList: ${wordList.length}");
-
-    if (wordList.isEmpty) {
-      print("wordListは空です: ${wordList.length}");
-      ShareProvider().getData(ref);
-      print("wordList: ${wordList.length}");
-    }
+    final future = ref.watch(futureWordListProvider);
+    final wordList = ref.watch(wordListProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -54,46 +56,53 @@ class MyHomePage extends ConsumerWidget {
             ),
           ),
           Flexible(
-            child: (wordList.isEmpty)
-                ? const Center(
-                    child: Text("なし"),
-                  )
-                : ListView.builder(
-                    itemCount: wordList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Dismissible(
-                        onDismissed: (direction) {
-                          //directionとは
-                          wordList.removeAt(index);
-                          ref.read(wordListProvider.notifier).state = [
-                            ...wordList
-                          ];
-                          ShareProvider().setData(wordList);
-                        },
-                        key: UniqueKey(),
-                        child: Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Card(
-                            elevation: 10,
+            child: future.when(
+              data: (data) {
+                return (data.isEmpty)
+                    ? const Text("Todoなし")
+                    : ListView.builder(
+                        itemCount: wordList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Dismissible(
+                            onDismissed: (direction) async {
+                              wordList.removeAt(index);
+                              ref.read(wordListProvider.notifier).state = [
+                                ...wordList
+                              ];
+                              await ref.read(shareProvider).setData(wordList);
+                            },
+                            key: UniqueKey(),
                             child: Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: Text(wordList[index]),
+                              padding: const EdgeInsets.all(2.0),
+                              child: Card(
+                                elevation: 10,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(18.0),
+                                  child: Text(wordList[index]),
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
+                          );
+                        },
                       );
-                    }),
+              },
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+              error: (Object error, StackTrace? stackTrace) => Center(
+                child: Text("エラー: $error"),
+              ),
+            ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
           if (_textEditingController.text != "") {
             wordList.add(_textEditingController.text);
             ref.read(wordListProvider.notifier).state = [...wordList];
-            //保存
-            print("保存前のwordListの中身: $wordList");
-            ShareProvider().setData(wordList);
+            print(wordList);
+            await ref.read(shareProvider).setData(wordList);
           }
         },
         child: const Icon(Icons.add),
--- lib/providers/shareProvider.dart
+++ lib/providers/shareProvider.dart
@@ -2,7 +2,7 @@ import 'package:riverpod_practice/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
+final shareProvider = Provider((ref) => ShareProvider());
 
 class ShareProvider {
 
@@ -13,16 +13,13 @@ class ShareProvider {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setStringList("todos", wordList);
 
-    //TODO 保存されてはいる
-    final check = prefs.getStringList("todos") ?? ["入ってないよ"];
-    print("チェック: $check");
   }
 
   //ロード
-  Future<void> getData(WidgetRef ref) async {
+  Future<List<String>> getData() async {
     final prefs = await SharedPreferences.getInstance();
     //保存されているリストがある場合、ない場合で場合分する必要がある
-    final returnWordList = prefs.getStringList("todos") ?? ["入ってないよ"];
-    ref.read(wordListProvider.notifier).state = returnWordList;
+    return prefs.getStringList("todos") ?? [];
+
   }
 }

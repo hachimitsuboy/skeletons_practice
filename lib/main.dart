31c0add58c7bec2177639dff2db4ad0e264066e1
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-08-06 13:52:18 +0900

    skeletonsパッケージとFutureProviderの組み合わせ

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_practice/providers/shareProvider.dart';
 import 'package:skeletons/skeletons.dart';
 
+
 FutureProvider<List<String>> futureWordListProvider =
     FutureProvider((ref) async {
   //shared_preferencesから保存されているwordListを取り出す
